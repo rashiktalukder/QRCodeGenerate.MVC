2848b5f50acec41c6fc326ed3075a6cea99e6995
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2024-10-29 23:30:47 +0600

    Add project files.

---
+++ QRCodeGenerate.MVC/Controllers/QRCodeGeneratorController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCodeGenerate.MVC.Models;
+using QRCoder;
+using System.Drawing.Imaging;
+using System.Drawing;
+using static QRCoder.PayloadGenerator;
+
+namespace QRCodeGenerate.MVC.Controllers
+{
+    public class QRCodeGeneratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            QrCodeModel qrCodeModel = new QrCodeModel();
+
+            return View(qrCodeModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(QrCodeModel qrCodeModel)
+        {
+            Payload payload = PayloadBasedOnQrCodeType(qrCodeModel);
+
+            QRCodeGenerator qrCodeGen = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrCodeGen.CreateQrCode(payload);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            string base64Str = Convert.ToBase64String(BitmapToByteArr(qrCodeAsBitmap));
+            qrCodeModel.QRImageURL = "data:image/png;base64," + base64Str;
+
+            return View("Index",qrCodeModel);
+        }
+
+        public Payload PayloadBasedOnQrCodeType(QrCodeModel qrCodeModel) 
+        {
+            Payload payload = null;
+
+            if(qrCodeModel.QrCodeType == "website")
+            {
+                payload = new Url(qrCodeModel.WebsiteURL);
+            }
+            else if(qrCodeModel.QrCodeType == "bookmark")
+            {
+                payload = new Bookmark(qrCodeModel.BookmarkURL, qrCodeModel.BookmarkURL);
+            }
+            else if (qrCodeModel.QrCodeType == "sms")
+            {
+                payload = new SMS(qrCodeModel.SMSPhoneNumber, qrCodeModel.SMSBody);
+            }
+            else if (qrCodeModel.QrCodeType == "whatsApp")
+            {
+                payload = new WhatsAppMessage(qrCodeModel.WhatsAppNumber, qrCodeModel.WhatsAppMessage);
+            }
+            else if (qrCodeModel.QrCodeType == "email")
+            {
+                payload = new Mail(qrCodeModel.ReceiverEmailAddress, qrCodeModel.EmailSubject, qrCodeModel.EmailMessage);
+            }
+            else
+            {
+                payload = new WiFi(qrCodeModel.WIFIName, qrCodeModel.WIFIPassword, WiFi.Authentication.WPA);
+            }
+
+            return payload;
+        }
+
+        private byte[] BitmapToByteArr(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ QRCodeGenerate.MVC/Models/QrCodeModel.cs
@@ -0,0 +1,32 @@
+﻿namespace QRCodeGenerate.MVC.Models
+{
+    public class QrCodeModel
+    {
+        public string? QrCodeType {  get; set; }
+        public string? QRImageURL { get; set; }
+
+        //for bookmark qr code
+        public string? BookmarkTitle { get; set; }
+        public string? BookmarkURL { get; set; }
+
+        // for email qr codes
+        public string? ReceiverEmailAddress { get; set; }
+        public string? EmailSubject { get; set; }
+        public string? EmailMessage { get; set; }
+
+        //for sms qr codes
+        public string? SMSPhoneNumber { get; set; }
+        public string? SMSBody { get; set; }
+
+        //for website
+        public string? WebsiteURL { get; set; }
+
+        // for whatsapp qr message code
+        public string? WhatsAppNumber { get; set; }
+        public string? WhatsAppMessage { get; set; }
+
+        // for wifi qr code
+        public string? WIFIName { get; set; }
+        public string? WIFIPassword { get; set; }
+    }
+}
